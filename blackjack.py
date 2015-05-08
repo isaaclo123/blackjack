38540e5b6743fe6dffefcbe1cb680e281472eb54
Isaac Lo <isaaclo123@gmail.com>
2015-05-07 22:45:46 -0700

    now mostly playable

--- blackjack.py
+++ blackjack.py
@@ -1,20 +1,31 @@
 # -*- coding: utf-8 -*-
 
 from random import randint
+from numpy import character
 
 def shuffle():
     global cards
-    cards = {"A♠":1, "2♠":2, "3♠":3, "4♠":4, "5♠":5, "6♠":6, "7♠":7, "8♠":8, "9♠":9, "10♠":10, "J♠":10, "Q♠":10, "K♠":10, "A♥":1, "2♥":2, "3♥":3, "4♥":4, "5♥":5, "6♥":6, "7♥":7, "8♥":8, "9♥":9, "10♥":10, "J♥":10, "Q♥":10, "K♥":10, "A♦":1, "2♦":2, "3♦":3, "4♦":4, "5♦":5, "6♦":6, "7♦":7, "8♦":8, "9♦":9, "10♦":10, "J♦":10, "Q♦":10, "K♦":10, "A♣":1, "2♣":2, "3♣":3, "4♣":4, "5♣":5, "6♣":6, "7♣":7, "8♣":8, "9♣":9, "10♣":10, "J♣":10, "Q♣":10, "K♣":10}
+    cards = {"A♠":0, "2♠":2, "3♠":3, "4♠":4, "5♠":5, "6♠":6, "7♠":7, "8♠":8, "9♠":9, "10♠":10, "J♠":10, "Q♠":10, "K♠":10, "A♥":0, "2♥":2, "3♥":3, "4♥":4, "5♥":5, "6♥":6, "7♥":7, "8♥":8, "9♥":9, "10♥":10, "J♥":10, "Q♥":10, "K♥":10, "A♦":0, "2♦":2, "3♦":3, "4♦":4, "5♦":5, "6♦":6, "7♦":7, "8♦":8, "9♦":9, "10♦":10, "J♦":10, "Q♦":10, "K♦":10, "A♣":0, "2♣":2, "3♣":3, "4♣":4, "5♣":5, "6♣":6, "7♣":7, "8♣":8, "9♣":9, "10♣":10, "J♣":10, "Q♣":10, "K♣":10}
 
 def deal(player, dealnum):
     global cards
-    if len(cards) <= 0 :
+    if len(cards) <= 1 :
         shuffle()
-    if player[2] == True :
+    if player[2] == True:
         for x in xrange (0,dealnum) :
             therandomint = randint(0,len(cards) - 1)
             player[0] = player[0] + cards.keys()[therandomint] + " "
             player[1] = player[1] + cards[cards.keys()[therandomint]]
+            if cards[cards.keys()[therandomint]] == 0 or player[1] > 21:
+                if player[1] + 11 <= 21 :
+                    player[1] = player[1] + 11
+                    player[3] = player[3] + 1
+                elif player[1] + 11 > 21 :
+                    if player[1] + 1 <= 21 :
+                        player[1] = player[1] + 1
+                    elif player[1] + 1 > 21 and player[3] > 0 :
+                        player[1] = player[1] - 10
+                        player[3] = player[3] - 1
             cards.pop(cards.keys()[therandomint])
             if player[1] > 21 :
                 player[0] = player[0] + "bust"
@@ -29,17 +40,32 @@ def dealer():
 def reset():
     global character
     global dealer
-    global noshow
-    character = ["", 0, True]
+    character = ["", 0, True, 0]
     character = deal(character,2)
-    dealer = ["", 0, True]
+    dealer = ["", 0, True, 0]
     dealer = deal(dealer,2)
-    noshow = True
 
 def thedealer():
     if character[2] == True :
-            while (dealer[1] < character[1]) & (dealer[2] == True):
+            while (dealer[1] < character[1]) and (dealer[2] == True):
                 deal(dealer,1)
+                
+def checkwin():
+    global character
+    global dealer
+    if ((character[1] > dealer[1]) and character[2] == True) or (character[2] == True and dealer[2] == False) :
+        print "You have won!\n"
+    elif ((character[1] < dealer[1]) and dealer[2] == True) or (character[2] == False and dealer[2] == True):
+        print "The dealer has won!\n"
+    elif dealer[1] == character[1] or (dealer[2] == False and character[2] == False) :
+        print "Tie\n"
+        
+def stay():
+    thedealer()
+    printout(False)
+    checkwin()
+    raw_input("Press Enter to continue...")
+    reset()
 
 def printout(hide):
     global character
@@ -62,14 +88,13 @@ shuffle()
 reset()
 while True :
     #while True :
-    printout(noshow)
+    printout(True)
     theinput = raw_input('command: ')
     print "\n" * 100
     if str(theinput) == "h" :
         deal(character,1)
     elif str(theinput) == "s" :
-        thedealer()
-        noshow = False
+        stay()
     elif str(theinput) == "r" :
         reset()
     elif str(theinput) == "q" :
